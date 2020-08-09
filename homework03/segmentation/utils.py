33da9a03c449d0f1ce3e7c2a1c4e51495edd9a1f
Tadej <tadej.svetina@gmail.com>
2020-08-09 13:52:38 +0200

    Added image augmentation

--- homework03/segmentation/utils.py
+++ homework03/segmentation/utils.py
@@ -1,3 +1,6 @@
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+
 from .data import SegmentationDataset
 
 def train_val_set(transform_train=None, transform_val=None):
@@ -13,3 +16,43 @@ def train_val_set(transform_train=None, transform_val=None):
     val_set = SegmentationDataset(val_folder_img, val_folder_mask, transform_val)
     
     return train_set, val_set
+
+def spatial_aug():
+    """Returns the list of spatial augmentations"""
+    
+    augs = [
+        A.VerticalFlip(),
+        A.HorizontalFlip(),
+        A.ElasticTransform(p=0.75),
+        A.Transpose(),
+        A.Rotate(limit=180, p=1),
+        A.RandomSizedCrop(min_max_height=(360,400), height=384, width=384),
+    ]
+    
+    return augs
+    
+def color_aug():
+    """Returns the list of color augmentations"""
+    
+    augs = [
+        A.GaussianBlur(),
+        A.HueSaturationValue(),
+    ]
+    
+    return augs
+    
+def processing_aug():
+    """A list of Float and ToTensor augmentations"""
+    
+    augs = [
+        A.ToFloat(),
+        ToTensorV2()
+    ]
+    
+def prepare_data_loaders():
+    """Return the data loaders for train and validation sets. Train set had spatial and color augmentations applied."""
+    
+    train_augs = A.Compose(spatial_aug() + color_aug() + processing_aug())
+    val_augs = A.Compose(processing_aug())
+    
+    train_set, val_set = train_val_set(train_augs, val_augs)
